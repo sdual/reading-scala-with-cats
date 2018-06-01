98fabb2226eda698f258d2306d8f2fc689187374
stenoritama <shr.nrht.0623@gmail.com>
2018-06-02 03:28:54 +0900

    add type class example

---
+++ src/main/scala/chap01/Json.scala
@@ -0,0 +1,12 @@
+package chap01
+
+sealed trait Json
+
+final case class JsObject(get: Map[String, Json]) extends Json
+final case class JsString(get: String) extends Json
+final case class JsNumber(get: Double) extends Json
+case object JsNull extends Json
+
+trait JsonWriter[A] {
+  def write(value: A): Json
+}
